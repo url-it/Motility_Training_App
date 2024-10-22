894be1ad16779b8235fa9e1b261c5ffb4b884d55
Uziel <urlaweblink@gmail.com>
2024-10-21 22:55:29 -0400

    Pushing to fix the os path finder to find the about.html document

--- bin/about.py
+++ bin/about.py
@@ -1,11 +1,16 @@
 from ipywidgets import Output
 from IPython.display import display, HTML
-
+import os
 class AboutTab(object):
 
     def __init__(self):
         # self.tab = Output(layout={'height': '600px'})
         self.tab = Output(layout={'height': 'auto'})
         # Change
-        self.tab.append_display_data(HTML(filename='../Motility_Training_App/doc/about.html'))
+        # self.tab.append_display_data(HTML(filename='../Motility_Training_App/doc/about.html'))
+        # Another change
+        # For some reason the path is not working online so we need to use the os module to get the path
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        about_html_path = os.path.abspath(os.path.join(base_path, '../doc/about.html'))
+        self.tab.append_display_data(HTML(filename=about_html_path))
         
